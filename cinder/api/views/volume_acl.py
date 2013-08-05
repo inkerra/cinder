17c87f29064b36fa4112d647ecb9bb88b095a157
inkerra <inkerra@gmail.com>
2013-08-05 15:22:06 -0400

    Add Volume ACL Permissions.
    blueprint volume-acl
    
    Change-Id: I7e04f31e32ce882241726776c79e14a151a79325

---
+++ cinder/api/views/volume_acl.py
@@ -0,0 +1,99 @@
+# Copyright (C) 2013 Mirantis Inc.
+# All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+from cinder.api import common
+from cinder.openstack.common import log as logging
+
+
+LOG = logging.getLogger(__name__)
+
+
+class ViewBuilder(common.ViewBuilder):
+    """Model volume_acl_permission API responses as a python dictionary."""
+
+    _collection_name = "volume_acl_permissions"
+
+    def __init__(self):
+        """Initialize view builder."""
+        super(ViewBuilder, self).__init__()
+
+    def summary_list(self, request, volume_acl_permissions):
+        """Show a list of volume permissions without many details."""
+        return self._list_view(self.summary, request, volume_acl_permissions)
+
+    def detail_list(self, request, volume_acl_permissions):
+        """Detailed view of a list of volume_acl_permissions ."""
+        return self._list_view(self.detail, request, volume_acl_permissions)
+
+    def summary(self, request, volume_acl_permission):
+        """Generic, non-detailed view of a volume_acl_permission."""
+        return {
+            'volume_acl_permission': {
+                'id': volume_acl_permission.get('id'),
+                'volume_id': volume_acl_permission.get('volume_id'),
+                'type': volume_acl_permission.get('type'),
+                'user_or_group_id':
+                volume_acl_permission.get('user_or_group_id'),
+                'access_permission':
+                volume_acl_permission.get('access_permission'),
+                'links': self._get_links(request, volume_acl_permission['id']),
+            },
+        }
+
+    def detail(self, request, volume_acl_permission):
+        """Detailed view of a single volume permission."""
+        return {
+            'volume_acl_permission': {
+                'id': volume_acl_permission.get('id'),
+                'created_at': volume_acl_permission.get('created_at'),
+                'volume_id': volume_acl_permission.get('volume_id'),
+                'type': volume_acl_permission.get('type'),
+                'user_or_group_id':
+                volume_acl_permission.get('user_or_group_id'),
+                'access_permission':
+                volume_acl_permission.get('access_permission'),
+                'links': self._get_links(request, volume_acl_permission['id']),
+            }
+        }
+
+    def create(self, request, volume_acl_permission):
+        """Detailed view of a single volume permission when created."""
+        return {
+            'volume_acl_permission': {
+                'id': volume_acl_permission.get('id'),
+                'created_at': volume_acl_permission.get('created_at'),
+                'volume_id': volume_acl_permission.get('volume_id'),
+                'type': volume_acl_permission.get('type'),
+                'user_or_group_id':
+                volume_acl_permission.get('user_or_group_id'),
+                'access_permission':
+                volume_acl_permission.get('access_permission'),
+                'links': self._get_links(request, volume_acl_permission['id'])
+            }
+        }
+
+    def _list_view(self, func, request, volume_acl_permissions):
+        """Provide a view for a list of volume permissions."""
+        vol_perms_list = [func(request, p)['volume_acl_permission']
+                          for p in volume_acl_permissions]
+        vol_perms_links = self._get_collection_links(request,
+                                                     volume_acl_permissions,
+                                                     self._collection_name)
+        vol_perms_dict = dict(volume_acl_permissions=vol_perms_list)
+
+        if vol_perms_links:
+            vol_perms_dict['volume_acl_permissions_links'] = vol_perms_links
+
+        return vol_perms_dict
---
+++ cinder/tests/test_volume_acl.py
@@ -0,0 +1,137 @@
+# vim: tabstop=4 shiftwidth=4 softtabstop=4
+
+# Copyright (c) 2013 Mirantis Inc.
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+"""Unit Tests for volume acl."""
+
+
+import datetime
+
+from cinder import context
+from cinder import db
+from cinder import exception
+from cinder.openstack.common import log as logging
+from cinder import test
+from cinder.volume.volume_acl import api as volume_acl_api
+
+
+LOG = logging.getLogger(__name__)
+
+
+class VolumeACLPermissionTestCase(test.TestCase):
+    """Test cases for volume ACL code."""
+    def setUp(self):
+        super(VolumeACLPermissionTestCase, self).setUp()
+        self.ctxt = context.RequestContext(user_id='user_id',
+                                           project_id='project_id')
+
+    def _create_volume(self, volume_id, status='available',
+                       user_id=None, project_id=None, ctxt=None):
+        if not ctxt:
+            ctxt = self.ctxt
+        if user_id is None:
+            user_id = ctxt.user_id
+        if project_id is None:
+            project_id = ctxt.project_id
+        vol = {'id': volume_id,
+               'updated_at': datetime.datetime(1, 1, 1, 1, 1, 1),
+               'user_id': user_id,
+               'project_id': project_id,
+               'display_name': 'Display Name',
+               'display_description': 'Display Description',
+               'size': 1,
+               'status': status}
+        volume = db.volume_create(ctxt, vol)
+        return volume
+
+    def test_volume_permission_create_update_delete(self):
+        acl_api = volume_acl_api.API()
+        volume = self._create_volume('1')
+        owner_perm_id = 1
+        auto_perm = db.volume_permission_get(self.ctxt, owner_perm_id)
+        self.assertEquals(auto_perm.volume_id, volume.id)
+        self.assertEquals(auto_perm.user_or_group_id, self.ctxt.user_id)
+        self.assertEquals(auto_perm.access_permission, 7)
+        new_access_permission = 7
+        new_user = 'new_user_id'
+        new_perm = acl_api.create(self.ctxt, volume.id, new_user,
+                                  access_permission=new_access_permission)
+        self.assertEquals(new_perm.access_permission, new_access_permission)
+        self.assertEquals(new_perm.type, 'user')
+        self.assertEquals(new_perm.user_or_group_id, new_user)
+
+        new_owner_permission = 4
+        self.assertRaises(exception.WrongAccessPermissionLevel,
+                          acl_api.create,
+                          self.ctxt, volume.id, self.ctxt.user_id,
+                          access_permission=new_owner_permission)
+        nctxt = context.RequestContext(user_id=new_user,
+                                       project_id=self.ctxt.project_id)
+        self.assertRaises(exception.WrongAccessPermissionLevel,
+                          acl_api.create,
+                          nctxt, volume.id, self.ctxt.user_id,
+                          access_permission=new_owner_permission)
+        upd_access_permission = 1
+        new_perm = acl_api.create(nctxt, volume.id, new_user,
+                                  access_permission=upd_access_permission)
+        self.assertEquals(new_perm.access_permission, upd_access_permission)
+        self.assertEquals(new_perm.type, 'user')
+        self.assertEquals(new_perm.user_or_group_id, new_user)
+        perm = acl_api.create(context.get_admin_context(), volume.id,
+                              self.ctxt.user_id,
+                              access_permission=new_owner_permission)
+        self.assertEquals(perm.id, owner_perm_id)
+        self.assertEquals(perm.access_permission, new_owner_permission)
+        acl_api.delete(self.ctxt, new_perm.id)
+        self.assertRaises(exception.WrongAccessPermissionLevel,
+                          acl_api.delete,
+                          self.ctxt, owner_perm_id)
+        acl_api.delete(context.get_admin_context(), owner_perm_id)
+
+    def test_volume_permission_create_not_found_volume(self):
+        acl_api = volume_acl_api.API()
+        self.assertRaises(exception.VolumeNotFound,
+                          acl_api.create,
+                          self.ctxt, '1', 'new_user_id',
+                          access_permission='7')
+
+    def test_volume_permission_get(self):
+        acl_api = volume_acl_api.API()
+        volume = self._create_volume('1')
+        owner_perm_id = 1
+        auto_perm = acl_api.get(self.ctxt, owner_perm_id)
+        self.assertEquals(auto_perm['id'], owner_perm_id)
+        new_access_permission = 7
+        new_user = 'new_user_id'
+        created_perm = acl_api.create(self.ctxt, volume.id, new_user,
+                                      access_permission=new_access_permission)
+        new_perm = acl_api.get(self.ctxt, created_perm.id)
+        self.assertEquals(created_perm.id, new_perm['id'])
+        volume2 = self._create_volume('2')
+        volume3 = self._create_volume('3', ctxt=context.get_admin_context())
+        self.assertEquals(volume3.id, '3')
+        perms = acl_api.get_all(self.ctxt)
+        self.assertEquals(len(perms), 3)
+        perms_for_vol1 = acl_api.get_all_by_volume(self.ctxt, volume.id)
+        self.assertEquals(len(perms_for_vol1), 2)
+        perms_for_vol2 = acl_api.get_all_by_volume(self.ctxt, volume2.id)
+        self.assertEquals(len(perms_for_vol2), 1)
+        all_perms = acl_api.get_all(context.get_admin_context())
+        self.assertEquals(len(all_perms), 4)
+
+        # volume permissions of volume3 (created by admin) is 4
+        self.assertRaises(exception.WrongAccessPermissionLevel,
+                          acl_api.get,
+                          self.ctxt, '4')
+        perm = acl_api.get(context.get_admin_context(), 4)
+        self.assertEquals(perm['id'], 4)
